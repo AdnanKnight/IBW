08a08c4352719017556c57a160a728d35310546b
AdnanKnight <adnan555legend@gmail.com>
2025-10-06 15:53:18 +0530

    Tweaks

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT
 
 // Middlewares
 const corsOptions = {
-    origin: ["https://ibw-frontend.onrender.com","https://islamicbookworld.in/", "http://localhost:5173", "http://localhost:5174"], // frontend URL
+    origin: ["https://ibw-frontend.onrender.com","https://islamicbookworld.in", "http://localhost:5173", "http://localhost:5174"], // frontend URL
     credentials: true,               // allow cookies to be sent
 };
 app.use(express.json());
